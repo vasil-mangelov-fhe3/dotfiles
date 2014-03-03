fed9be9c59b5bdcc1aed4f2357f01eb3cdeb6246
Steven Koeberich <skoeberich@nrecom.net>
2014-03-03 18:19:05 +0100

    ...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 # Paste this into ssh
-# curl -sL https://repo.nrecom.net/nre-com-net/dotfiles/raw/master/bootstrap.sh | tar -xzO | /bin/bash -ex
+# curl -sL https://repo.nrecom.net/nre-com-net/dotfiles/raw/master/bootstrap.sh | /bin/bash
 # When forking, you can get the URL from the raw (<>) button.
 
 ### Set some command variables depending on whether we are root or not ###
 # This assumes you use a debian derivate, replace with yum, pacman etc.
 aptget='sudo apt-get'
 if [ `whoami` = 'root' ]; then
-	aptget='apt-get'
+    aptget='apt-get'
 fi
 
 ### Install git and some other tools we'd like to use ###
