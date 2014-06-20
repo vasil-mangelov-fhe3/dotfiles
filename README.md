61bea47cceebe5ca1efb3f67d8b6412b1fca8f7a
NRE-Backup <admins@nrecom.net>
2014-06-20 13:31:49 +0200

    updated for github

--- README.md
+++ README.md
@@ -3,21 +3,20 @@
 This is our compilation of dotfiles, they are build to be managed by [Anders Ingemann's homeshick](https://github.com/andsens/homeshick)
 All contribution goes to the original developers we only have put the pieces together.
 
-This includes [Lokaltog powerline-fonts](https://github.com/Lokaltog/powerline-fonts) which are installed to "$HOME/.fonts"
+This includes [Lokaltog powerline-fonts](https://github.com/Lokaltog/powerline-fonts) which are installed to "${HOME}/.fonts"
 
 ## Install
 
 ### Manual
 1. Install homeshick like in the [Readme](https://github.com/andsens/homeshick/blob/master/README.md)
-2. Add NRE.Com.Net Dotfiles to homeshick `homeshick clone https://repo.nrecom.net/nre-com-net/dotfiles.git`
-3. Add our bashrc to your existing `printf '\nsource "$HOME/.bashrc_homesick"' >> $HOME/.bashrc`
+2. Add NRE.Com.Net Dotfiles to homeshick `homeshick clone https://github.com/NemesisRE/dotfiles.git`
+3. Add our bashrc to your existing `printf '\nsource "${HOME}/.bashrc_homesick"' >> ${HOME}/.bashrc`
 
 ### Bootstraped
 **NOTE:**
-This will also install [revans bash-it](https://github.com/revans/bash-it) with some [NRE.Com.Net defaults](https://repo.nrecom.net/nre-com-net/bash-it/tree/master)
-and our [Vimfiles](https://repo.nrecom.net/nre-com-net/vimfiles)
+This will also install [revans bash-it](https://github.com/revans/bash-it) with some [NRE.Com.Net defaults](https://github.com/NemesisRE/dotfiles/tree/master/bash-it)
 
-1. `curl -sL https://repo.nrecom.net/nre-com-net/dotfiles/raw/master/bootstrap.sh | /bin/bash`
+1. `curl -sL https://raw.githubusercontent.com/NemesisRE/dinner/master/bootstrap.sh | /bin/bash`
 
 
 **NOTE:**
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
 # Paste this into ssh
-# curl -sL https://repo.nrecom.net/nre-com-net/dotfiles/raw/master/bootstrap.sh | /bin/bash
+# curl -sL https://raw.githubusercontent.com/NemesisRE/dinner/master/bootstrap.sh | /bin/bash
 # When forking, you can get the URL from the raw (<>) button.
 
 ### Set some command variables depending on whether we are root or not ###
 # This assumes you use a debian derivate, replace with yum, pacman etc.
-aptget='sudo apt-get'
-if [ `whoami` = 'root' ]; then
-    aptget='apt-get'
+if [ $(whoami) = 'root' ]; then
+	APT='apt-get'
+else
+	APT='sudo apt-get'
 fi
 
 ### Install git and some other tools we'd like to use ###
-$aptget update
-$aptget install -y tmux vim git screen htop
+${APT} update
+${APT} install -y tmux vim git screen htop
 
 ### Install homeshick ###
-git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-source $HOME/.homesick/repos/homeshick/homeshick.sh
+git clone git://github.com/andsens/homeshick.git ${HOME}/.homesick/repos/homeshick
+source ${HOME}/.homesick/repos/homeshick/homeshick.sh
 
-homeshick --batch clone https://repo.nrecom.net/nre-com-net/dotfiles.git
+homeshick --batch clone https://github.com/NemesisRE/dotfiles.git
 
 ### Link it all to $HOME ###
 homeshick link
 
 ### Install bash-it with NRE.Com.Net defaults ###
-$HOME/.homesick/repos/dotfiles/bash-it/install.sh defaults
+${HOME}/.homesick/repos/dotfiles/bash-it/install.sh defaults
 
 # Register powerline fonts
 fc-cache -fv
 
 # Source .bashrc_homesick in .bashrc
-grep -xq 'source "$HOME/.bashrc_homesick"' $HOME/.bashrc || printf '\nsource "$HOME/.bashrc_homesick"' >> $HOME/.bashrc
+grep -xq 'source "${HOME}/.bashrc_homesick"' ${HOME}/.bashrc || printf '\nsource "${HOME}/.bashrc_homesick"' >> ${HOME}/.bashrc
 
 echo "Relog to start your proper shell"
