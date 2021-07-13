ec309ef0113c545a37f0561923d17fb47c51d2c9
Steven Koeberich <steven.koeberich@fhe3.com>
2021-07-14 00:25:56 +0200

    added k9s config

--- shell/common/nredf_functions.sh
+++ shell/common/nredf_functions.sh
@@ -45,6 +45,10 @@ function _nredf_set_defaults() {
   export LANGUAGE=en_US.UTF-8
   export LC_ALL=en_US.UTF-8
 
+  export XDG_CONFIG_HOME="${HOME}/.config"
+  export XDG_CACHE_HOME="${HOME}/.cache"
+  export XDG_DATA_HOME="${HOME}/.local/share"
+
   export PATH=${HOME}/bin:${HOME}/.local/bin:/usr/local/bin:${PATH}
   [[ -d /snap/bin ]] && export PATH=${PATH}:/snap/bin
   export GOPATH=${HOME}/.local
