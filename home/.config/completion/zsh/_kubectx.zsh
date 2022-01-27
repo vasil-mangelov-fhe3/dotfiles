595610cec027487043e4c4dfffd31ebe3ce076b0
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2022-01-27 16:43:49 +0000

    fixes

--- home/.config/completion/zsh/_kubectx.zsh
+++ home/.config/completion/zsh/_kubectx.zsh
@@ -1,18 +1,20 @@
 #compdef kubectx kctx=kubectx
 
-local KUBECTX="${HOME}/.kube/kubectx"
-PREV=""
+__kubectx() {
+  local KUBECTX="${HOME}/.kube/kubectx"
+  PREV=""
 
-local all_contexts="$(kubectl config get-contexts --output='name')"
-if [ -f "$KUBECTX" ]; then
-    # show '-' only if there's a saved previous context
-    local PREV=$(cat "${KUBECTX}")
+  local all_contexts="$(kubectl config get-contexts --output='name')"
+  if [ -f "$KUBECTX" ]; then
+      # show '-' only if there's a saved previous context
+      local PREV=$(cat "${KUBECTX}")
 
-    _arguments \
-      "-d:*: :(${all_contexts})" \
-      "(- *): :(- ${all_contexts})"
-else
-    _arguments \
-      "-d:*: :(${all_contexts})" \
-      "(- *): :(${all_contexts})"
-fi
+      _arguments \
+        "-d:*: :(${all_contexts})" \
+        "(- *): :(- ${all_contexts})"
+  else
+      _arguments \
+        "-d:*: :(${all_contexts})" \
+        "(- *): :(${all_contexts})"
+  fi
+}
--- home/.config/completion/zsh/_kubens.zsh
+++ home/.config/completion/zsh/_kubens.zsh
@@ -1,2 +1,5 @@
 #compdef kubens kns=kubens
-_arguments "1: :(- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}'))"
+
+__kubens() {
+  _arguments "1: :(- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}'))"
+}
