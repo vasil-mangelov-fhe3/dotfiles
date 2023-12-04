d3cf93c0c6fbd000171c25e8d10790c56582304b
Steven "NemesisRE" Koeberich <nemesissre@gmail.com>
2023-12-04 12:58:32 +0100

    fixed

--- shell/common/functions/nredf_create_completion.bash
+++ shell/common/functions/nredf_create_completion.bash
@@ -1,18 +1,26 @@
+#!/usr/bin/env bash
+#
+# vim: ts=2 sw=2 et ff=unix ft=bash syntax=sh
+# shellcheck disable=SC2086
+
 function _nredf_create_completion() {
-  local BINARY=${1}
-  local COMPLETION_CMD=${2}
+  local BINARY="${1}"
+  local COMPLETION_CMD="${2}"
   local COMPLETION_FILE
 
-  case ${NREDF_SHELL_NAME} in
+  case "${NREDF_SHELL_NAME}" in
     bash) COMPLETION_FILE="${BINARY}.bash";;
     zsh) COMPLETION_FILE="_${BINARY}";;
     *) return 1;;
   esac
 
   if [[ -f "${XDG_BIN_HOME}/${BINARY}" ]]; then
-    "${XDG_BIN_HOME}/${BINARY}" "${COMPLETION_CMD}" 2>/dev/null > "${XDG_CONFIG_HOME}/completion/${NREDF_SHELL_NAME}/${COMPLETION_FILE}"
+    if "${XDG_BIN_HOME}/${BINARY}" ${COMPLETION_CMD} &>/dev/null; then
+      "${XDG_BIN_HOME}/${BINARY}" ${COMPLETION_CMD} 2>/dev/null > "${XDG_CONFIG_HOME}/completion/${NREDF_SHELL_NAME}/${COMPLETION_FILE}"
+    else
+      return 1
+    fi
   else
     return 1
   fi
-
 }
